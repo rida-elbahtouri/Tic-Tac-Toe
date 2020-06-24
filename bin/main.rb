31155cf0608fea35b3efe7ab945724eb0930f035
rida-elbahtouri <elbhatouri0123@gmail.com>
2020-06-24 09:59:38 +0100

    create some basic flow in the main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-puts 'hello world'
+
+puts 'Hello and welcome to Tic Tac Toe'
+puts 'can the first player tell me his name :'
+player1 = gets.chomp
+puts "nice to meet you #{player1} can the second player tell his name :"
+player2 = gets.chomp
+puts "Hello #{player1} and #{player2}"
+# random player will start
+# print the board
+# ask player to chose a number from the board
+puts " #{player1} chose a number from the board"
+# change the value of the number to X
+puts " #{player2} chose a number from the board"
+# change the value of the number to O
+# check if some one win or for draw
