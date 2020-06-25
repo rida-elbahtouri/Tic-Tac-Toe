def75a01b7883b815c15d5b774e94c413e3e77b0
rida-elbahtouri <elbhatouri0123@gmail.com>
2020-06-25 11:38:38 +0100

    add the draw condition

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,40 +1,40 @@
-module Logic
-  class CheckWinning
-    attr_writer :name, :mark, :position
-    def check_win(mark)
-      hor1 = @position[0..2].all?(mark)
-      hor2 = @position[3..5].all?(mark)
-      hor3 = @position[6..8].all?(mark)
-      ver1 = [@position[0], @position[3], @position[6]].all?(mark)
-      ver2 = [@position[1], @position[4], @position[7]].all?(mark)
-      ver3 = [@position[2], @position[5], @position[8]].all?(mark)
-      dia1 = [@position[0], @position[4], @position[8]].all?(mark)
-      dia2 = [@position[2], @position[4], @position[6]].all?(mark)
-      result = [hor1, hor2, hor3, ver1, ver2, ver3, dia1, dia2].any?(true)
-      result
-    end
-
-    def check_the_winer(player, mark)
-      msg = "congratulation #{player} you win the game" if check_win(mark)
-      msg
-    end
+# module Logic
+class CheckWinning
+  attr_writer :positions
+  def check_win(mark)
+    hor1 = @positions[0..2].all?(mark)
+    hor2 = @positions[3..5].all?(mark)
+    hor3 = @positions[6..8].all?(mark)
+    ver1 = [@positions[0], @positions[3], @positions[6]].all?(mark)
+    ver2 = [@positions[1], @positions[4], @positions[7]].all?(mark)
+    ver3 = [@positions[2], @positions[5], @positions[8]].all?(mark)
+    dia1 = [@positions[0], @positions[4], @positions[8]].all?(mark)
+    dia2 = [@positions[2], @positions[4], @positions[6]].all?(mark)
+    result = [hor1, hor2, hor3, ver1, ver2, ver3, dia1, dia2].any?(true)
+    result
   end
-  class CheckPosition
-    attr_writer :position, :input
-    def check_input
-      if @input.to_i in [1..9]
-        true
-      end
-    end
-    def check_position
-      if @position.class == Integer
-        true
-      end
-    end
+
+  def check_the_winer(player, mark)
+    msg = "congratulation #{player} you win the game" if check_win(mark)
+    msg
+  end
+
+  def check_draw(mark)
+    @positions.all?(String) and check_win(mark) == false
+  end
+  end
+class CheckPosition
+  attr_writer :position, :input
+  def check_input
+    true if @input.to_i.between?(1, 9)
+  end
+
+  def check_position
+    true if @position.class == Integer
   end
 end
+# end
 cond = CheckWinning.new
-cond.name = 'fff'
-cond.mark = 'O'
-cond.position = ['X', 2, 3, 4, 'X', 6, 7, 8, 'X']
+cond.positions = %w[X O X O X O O X O]
 puts cond.check_the_winer('rida', 'X')
+puts cond.check_draw('X')
