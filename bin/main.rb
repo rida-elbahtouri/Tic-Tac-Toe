1a40314d10c3d7513f390c0332145bf0b0ab144d
rida-elbahtouri <elbhatouri0123@gmail.com>
2020-06-26 09:18:07 +0100

    add Board class and optimize main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -2,14 +2,6 @@
 require_relative '../lib/logic.rb'
 positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-def draw_board(positions)
-  puts "#{positions[0]} | #{positions[1]} | #{positions[2]}"
-  puts '----------'
-  puts "#{positions[3]} | #{positions[4]} | #{positions[5]}"
-  puts '----------'
-  puts "#{positions[6]} | #{positions[7]} | #{positions[8]}"
-end
-
 def starting_of_thegame
   puts 'Hello and welcome to Tic Tac Toe'
   puts 'the game is for two players, X and O, who take turns marking the spaces in a 3×3 grid.'
@@ -19,9 +11,9 @@ def starting_of_thegame
   puts "nice to meet you #{player1} can the second player tell his name :"
   player2 = gets.chomp
   puts "Hello #{player1} and #{player2}"
-
-  players = [player1, player2]
-  puts "the first player will be #{players[0]} you will play with X"
+  players = [player1, player2].shuffle
+  puts 'the game will chose a random player to start'
+  puts "the first random player will be #{players[0]} you will play with X"
   puts 'this is the board of the game'
   players
 end
@@ -45,19 +37,19 @@ end
 game_on = true
 
 player = starting_of_thegame
-draw_board(positions)
+board = Board.new
+board.draw_board(positions)
 current_player = [player[0], 'X']
-a = 1
 i = 0
 while game_on
 
   chose_position(current_player[0], current_player[1], positions)
-  draw_board(positions)
+  board.draw_board(positions)
   result = CheckWinning.new
   result.positions = positions
   win = result.check_win(current_player[1])
   draw = result.check_draw(current_player[1])
-  if win # and/or draw (the exact condition in this milestone is not important)
+  if win
     puts result.check_the_winer(current_player[0])
     game_on = false
   elsif draw
@@ -66,8 +58,7 @@ while game_on
   else
     i = i == 1 ? 0 : 1
     mark = i == 1 ? 'O' : 'X'
-    p current_player = [player[i], mark]
-    a += 1
+    current_player = [player[i], mark]
   end
 
 end
--- lib/logic.rb
+++ lib/logic.rb
@@ -31,3 +31,12 @@ class CheckPosition
     true if position.class == Integer
   end
 end
+class Board
+  def draw_board(positions)
+    puts "#{positions[0]} | #{positions[1]} | #{positions[2]}"
+    puts '----------'
+    puts "#{positions[3]} | #{positions[4]} | #{positions[5]}"
+    puts '----------'
+    puts "#{positions[6]} | #{positions[7]} | #{positions[8]}"
+  end
+end
